bd5d61c37dfcca83fb509dfa6d352bdec213b531
Bryan Shin <bryanshin27@gmail.com>
2025-10-28 02:17:34 -0700

    matchwave

--- src/backend/core/config.py
+++ src/backend/core/config.py
@@ -77,7 +77,7 @@ MM_ACTIVITY_WINDOW_SECONDS = 15 * 60
 MM_PRUNE_INTERVAL_SECONDS = 60
 
 # Global matchmaking interval (matchwave) in seconds
-MM_MATCH_INTERVAL_SECONDS = 10
+MM_MATCH_INTERVAL_SECONDS = 45
 
 # Time window in seconds for players to abort a match after it's found
 MM_ABORT_TIMER_SECONDS = 180
